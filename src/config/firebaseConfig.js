c265f9ddb33b1f33ba639c0f61a0a06d13a99b61
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2021-07-23 17:18:06 +0200

    new config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,6 +1,6 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyDQoZ5pXeQMbpX-ZllBnGbaX4qNQOAI-sY",
-  authDomain: "bank-c4f41.web.app",
-  databaseURL: "https://bank-c4f41.firebaseio.com/",
-  storageBucket: "gs://bank-c4f41.appspot.com"
+  apiKey: "AIzaSyBpJqUsOHfK_pxxuipB3RWo9Bpq_xklxQ0",
+  authDomain: "bankorama-bank.web.app",
+  databaseURL: "https://bankorama-bank.firebaseio.com/",
+  storageBucket: "gs://bankorama-bank.appspot.com/"
 };
