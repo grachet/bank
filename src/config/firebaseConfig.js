2c5fa166fc7ce60604b4b9ef0e7d7e3517fd0c84
Guillaume Rachet <guillaume.rachet@gmail.com>
2021-07-23 17:21:27 +0200

    Delete firebaseConfig.js

--- src/config/firebaseConfig.js
+++
@@ -1,6 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: "AIzaSyBpJqUsOHfK_pxxuipB3RWo9Bpq_xklxQ0",
-  authDomain: "bankorama-bank.web.app",
-  databaseURL: "https://bankorama-bank.firebaseio.com/",
-  storageBucket: "gs://bankorama-bank.appspot.com/"
-};
